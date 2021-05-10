5c94727523b8811fe067e395ff447f72c5c68369
ajsm <codingportugal@gmail.com>
2021-05-10 22:47:51 +0100

    1 - Estrutura do projeto

---
+++ pages/header.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>OLX</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="assets/css/style.css"/>
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="./" class="navbar-brand">OLX</a>
+			</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="">Cadastre-se</a></li>
+				<li><a href="">Login</a></li>
+			</ul>
+		</div>
