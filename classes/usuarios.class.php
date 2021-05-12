12557e016afc2260a32448e4ac5483188da6303a
ajsm <codingportugal@gmail.com>
2021-05-12 14:33:13 +0100

    5 - Finalização do login e inicio dos meus-anuncios

--- classes/usuarios.class.php
+++ classes/usuarios.class.php
@@ -23,5 +23,22 @@ class Usuarios {
 			return false;
 		}
 	}
+
+	public function login($email, $senha){
+		global $pdo;
+
+		$sql = $pdo->prepare("SELECT id FROM usuarios WHERE email = :email AND senha = :senha");
+		$sql->bindValue(":email", $email);
+		$sql->bindValue(":senha", md5($senha));
+		$sql->execute();
+
+		if($sql->rowCount() > 0) {
+			$dado = $sql->fetch();
+			$_SESSION['cLogin'] = $dado['id'];
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 ?>
